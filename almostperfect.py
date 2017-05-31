fb475dff8928fc51bd5265ce4b53afbdf60544d4
MichalDurinik <michal.durinik2@mail.dcu.ie>
2017-05-31 03:36:15 +0100

    solved

--- almostperfect.py
+++ almostperfect.py
@@ -1,21 +1,20 @@
 #https://open.kattis.com/problems/almostperfect
 from sys import stdin
+from math import sqrt
+
+def factors(n):
+    a = [[i, n//i] for i in range(2, int(sqrt(num)) + 1) if n % i == 0]
+    return set([x for pair in a for x in pair])
 
 for num in stdin:
     num = int(num.strip())
-    
-    i = 2
-    total = 1
-    while i + total < num + 3:
-        if num % i == 0:
-            total += i
-        
-        i += 1
-            
+
+    #1 and num itself was excluded in factors
+    total = sum(factors(num)) + 1
+
     if total == num:
         print(num, "perfect")
-    elif total + 1 == num or total + 2 == num or total - 1 == num or total - 2 == num:
+    elif abs(total - num) <= 2:
         print(num, "almost perfect")
     else:
-        print(num, "not perfect")
-    
+        print(num, "not perfect") 
